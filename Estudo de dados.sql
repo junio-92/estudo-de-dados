ab3a06d2bf48c50ef0afc9406f95ade2356ccfd9
Junio Dias <9145junio@gmail.com>
2023-04-04 23:53:25 -0300

    dados

---
+++ Estudo de dados.sql
@@ -0,0 +1,19 @@
+create database preenchimento;
+use preenchimento;
+create table preenchimentos(
+
+nome varchar(30),
+idade tinyint ,
+peso float,
+rua varchar(30),
+bairro varchar (30),
+cidade varchar(30),
+estado varchar(30),
+regiao varchar(30),
+pais varchar(30),
+continente varchar(30)
+
+
+);
+
+describe preenchimentos;
