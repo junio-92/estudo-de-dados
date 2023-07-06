a94744c981b3d891bacbe1e804a72a905ef5940d
Junio Dias <9145junio@gmail.com>
2023-07-06 17:35:41 -0300

    revisar

---
+++ banco de dados/Revisao2.sql
@@ -0,0 +1,12 @@
+use universo;
+
+insert into junioherois
+
+( id, nome , nascimento,sexo, peso, altura, nacionalidade)
+values
+( default,'Zeltran' , '01-09-2011','M', '70.70', '1.83', 'brasileiro'),
+( default,'lazer' , '09-07-2002','M', '85.70', '1.93', 'brasileiro'),
+( default,'Telidio' , '12-07-2001','M', '75.70', '1.63', 'brasileiro');
+
+select * from junioverso;
+
